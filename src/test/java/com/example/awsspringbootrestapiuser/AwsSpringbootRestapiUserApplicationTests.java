bf4c940eb942ee6381fb2951cb78fc680ec96c90
devpremoli <prem.oli@bison.howard.edu>
2025-01-20 20:26:03 -0600

    spring project

---
+++ src/test/java/com/example/awsspringbootrestapiuser/AwsSpringbootRestapiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsspringbootrestapiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootRestapiUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
